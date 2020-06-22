9e655faf3fa7971bd8be11dff7bc494730e4ddbd
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2020-06-22 23:01:08 +0530

    render table component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Table from "./components/Table";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Dynamic Table</h3>
       </header>
+      <Table />
     </div>
   );
 }
